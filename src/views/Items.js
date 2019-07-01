e44f16cb96634e8e22420a15954558c3dd841611
Emilian Ćwiok <emilian.cw@gmail.com>
2019-07-01 11:16:02 +0200

    Use flatlist instead of map

--- src/views/Items.js
+++ src/views/Items.js
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, Image, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
 import Footer from '../components/Footer';
 import Background from '../components/Background'
 import { items } from '../assets/index';
-import Icon from 'react-native-vector-icons/FontAwesome5';
 import Health from '../assets/UI/health.png';
-import Cost from '../assets/UI/cost.png';
 import Mana from '../assets/UI/mana.png';
 import Damage from '../assets/UI/dps.png';
 import AttackSpeed from '../assets/UI/attackspeed.png';
-import AttackRange from '../assets/UI/attackrange.png';
 import MagicResist from '../assets/UI/magicresist.png';
 import AbilityPower from '../assets/UI/ap.png';
 import Armor from '../assets/UI/armor.png';
@@ -17,7 +14,7 @@ import Armor from '../assets/UI/armor.png';
 const Items = (props) => {
 
     const [recipes, setRecipes] = useState([]);
-    const [selected, setSelected] = useState('');
+    const [selected, setSelected] = useState();
     const [baseItems, setBaseItems] = useState([
         "B.F Sword",
         "Recurve Bow",
@@ -30,63 +27,16 @@ const Items = (props) => {
     ])
 
     showRecipes = (item, index) => {
+        const foundRecipes = Object.keys(items[item].recipes).map(el => {
+            return items[el];
+        })
         setSelected(index);
-        setRecipes(items[item].recipes);
+        setRecipes(foundRecipes);
     }
 
     useEffect(() => {
-        showRecipes("B.F Sword", 0)
+        showRecipes("B.F Sword", 0);
     }, [])
-
-
-    let recipeData = Object.keys(recipes).map((el, index) => {
-        return <View key={el} style={styles.itemWrapper}>
-            <Image source={items[recipes[el]].image} style={styles.baseItemImage} />
-            <Image source={items[el].image} style={styles.itemImage} />
-            <View style={{ marginHorizontal: 10 }}>
-                {items[el].ad &&
-                    <View style={{ flexDirection: "row" }}>
-                        <Image source={Damage} />
-                        <Text style={styles.iconText}>{items[el].ad}</Text>
-                    </View>
-                }
-                {items[el].ap &&
-                    <View style={{ flexDirection: "row" }}>
-                        <Image source={AbilityPower} />
-                        <Text style={styles.iconText}>{items[el].ap}</Text>
-                    </View>}
-                {items[el].mr &&
-                    <View style={{ flexDirection: "row" }}>
-                        <Image source={MagicResist} />
-                        <Text style={styles.iconText}>{items[el].mr}</Text>
-                    </View>}
-                {items[el].armor &&
-                    <View style={{ flexDirection: "row" }}>
-                        <Image source={Armor} />
-                        <Text style={styles.iconText}>{items[el].armor}</Text>
-                    </View>}
-                {items[el].health &&
-                    <View style={{ flexDirection: "row" }}>
-                        <Image source={Health} />
-                        <Text style={styles.iconText}>{items[el].health}</Text>
-                    </View>}
-                {items[el].as &&
-                    <View style={{ flexDirection: "row" }}>
-                        <Image source={AttackSpeed} />
-                        <Text style={styles.iconText}>{items[el].as}</Text>
-                    </View>}
-                {items[el].mana &&
-                    <View style={{ flexDirection: "row" }}>
-                        <Image source={Mana} />
-                        <Text style={styles.iconText}>{items[el].mana}</Text>
-                    </View>}
-            </View>
-            <View style={{ flexShrink: 1 }}>
-                <Text style={{ color: "#00FF00", fontSize: 18 }}>{items[el].name}</Text>
-                <Text style={{ color: "#F0E68C" }}>{items[el].passive}</Text>
-            </View>
-        </View >
-    })
 
     isSelected = (ind) => {
         return selected === ind ? styles.topImageSelected : styles.topImage;
@@ -103,7 +53,52 @@ const Items = (props) => {
                         </TouchableOpacity>
                     })}
                 </View>
-                {recipeData}
+                <FlatList data={recipes} renderItem={({ item, index }) => <View style={styles.itemWrapper}>
+                    <Image source={items[baseItems[index]].image} style={styles.baseItemImage} />
+                    <Image source={item.image} style={styles.itemImage} />
+                    <View style={{ marginHorizontal: 10 }}>
+                        {item.ad &&
+                            <View style={{ flexDirection: "row" }}>
+                                <Image source={Damage} />
+                                <Text style={styles.iconText}>{item.ad}</Text>
+                            </View>
+                        }
+                        {item.ap &&
+                            <View style={{ flexDirection: "row" }}>
+                                <Image source={AbilityPower} />
+                                <Text style={styles.iconText}>{item.ap}</Text>
+                            </View>}
+                        {item.mr &&
+                            <View style={{ flexDirection: "row" }}>
+                                <Image source={MagicResist} />
+                                <Text style={styles.iconText}>{item.mr}</Text>
+                            </View>}
+                        {item.armor &&
+                            <View style={{ flexDirection: "row" }}>
+                                <Image source={Armor} />
+                                <Text style={styles.iconText}>{item.armor}</Text>
+                            </View>}
+                        {item.health &&
+                            <View style={{ flexDirection: "row" }}>
+                                <Image source={Health} />
+                                <Text style={styles.iconText}>{item.health}</Text>
+                            </View>}
+                        {item.as &&
+                            <View style={{ flexDirection: "row" }}>
+                                <Image source={AttackSpeed} />
+                                <Text style={styles.iconText}>{item.as}</Text>
+                            </View>}
+                        {item.mana &&
+                            <View style={{ flexDirection: "row" }}>
+                                <Image source={Mana} />
+                                <Text style={styles.iconText}>{item.mana}</Text>
+                            </View>}
+                    </View>
+                    <View style={{ flexShrink: 1 }}>
+                        <Text style={{ color: "#00FF00", fontSize: 18 }}>{item.name}</Text>
+                        <Text style={{ color: "#F0E68C" }}>{item.passive}</Text>
+                    </View>
+                </View >} />
             </ScrollView>
             <Footer navigation={props.navigation} />
         </View >
